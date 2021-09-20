7cbc30b54c18ec4294d211bc749738f63f3f3cfb
Akins <82357502+Akins2229@users.noreply.github.com>
2021-09-19 22:22:26 -0500

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,36 @@
+import discord
+
+from discord_slash import SlashContext
+
+MustBePassedInGuildError:
+  def __init__(self, command_name: str) -> None:
+    self.message="Command {} must be passed within a guild (server)."
+  
+  def __repr__(self) -> str:
+    return self.message
+    
+async def get_setup_type(
+  ctx: SlashContext,
+  command_name: str,
+  member: str
+) -> int:
+  if not ctx.guild:
+    raise MustBePassedInGuild(command_name)
+  
+  else:
+    if str(ctx.guild.id) not in db["guilds"]:
+      return ctx.channel.id
+    
+    elif db["guilds"][str(ctx.guild.id)] == 1:
+      channel = discord.get_channel(db["guilds"][str(ctx.guild.id)]["channel"])
+      thread = await channel.create_thread(name="{0} v. {1}".format(ctx.author.display_name, member.display_name))
+      return thread
+    
+    elif db["guilds"][str(ctx.guild.id)] == 2:
+      channel = discord.get_channel(db["guilds"][str(ctx.guild.id)]["channel"])
+      return channel
+    
+    elif db["guilds"][str(ctx.guild.id)] == 3:
+      category = discord.utils.get(ctx.guild.categories, id=db["guilds"][str(ctx.guild.id)]["category"])
+      channel = await ctx.guild.create_text_channel("{} v. {}".fromat(ctx.author.display_name, member.display_name), category=category)
+      return channel
