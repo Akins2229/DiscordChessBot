3ba6789b485ff795c2353fe20b002b173a686ca0
Akins <82357502+Akins2229@users.noreply.github.com>
2021-11-03 11:41:22 -0500

    Update chess.py

--- plugins/chess/chess.py
+++ plugins/chess/chess.py
@@ -4,12 +4,13 @@ Copyright 2021 Akins
 
 Apache License 2.0
 
-ChessBot v7.0
+ChessBot v7.1
 
 """
 
 import typing
 import random
+import asyncio
 
 import discord
 from discord.ext import commands
@@ -18,7 +19,6 @@ from plugins.chess.data import Game, Participant
 
 import utils
 
-from discord_slash import cog_ext, SlashContext
 from discord_slash.model import ButtonStyle
 from discord_slash.utils.manage_components import create_button, create_actionrow, wait_for_component
 
@@ -41,21 +41,15 @@ class Chess(commands.Cog):
   def __init__(self, bot: commands.Bot):
     self.bot=bot
     
-  @cog_ext.cog_slash(
+  @commands.command(
     name="play",
     description="Allows you to play a match of chess against another user.",
-    options = [
-      {
-        "name": "member",
-        "description": "Member you would like to play Chess against.",
-        "type": 6,
-        "required": True
-      } 
-    ]
+    brief="Allows you to play chess against someone.",
+    usage="<member>"
   )
   async def _play(
     self,
-    ctx: SlashContext,
+    ctx: commands.Context,
     member: discord.Member
   ) -> discord.Message:
 
@@ -72,16 +66,21 @@ class Chess(commands.Cog):
     
 
     message_obj = await ctx.channel.send(
-      "{0}, would you like to play chess against {1}?".format(member.mention, ctx.author.mention),
-      components=[
-        action_row
-      ]
+      "{0}, would you like to play chess against {1}?".format(member.mention, ctx.author.mention)
     )
     
-    component = await wait_for_component(self.bot, components=action_row)
+    emojis = ["✅", "❎"]
     
-    if component.custom_id == "no":
-      return await ctx.send("Request to play chess denied.")
+    for emoji in emojis:
+      message_obj.add_reaction(emoji)
+    
+    try:
+      component = await self.bot.wait_for('reaction_add', check = lambda reaction, user: reaction.message == message_obj and reaction.emoji in emojis and user == [game.white.user, game.black.user].remove(ctx.author), timeout=180)
+    except asyncio.TimeOutError:
+      await ctx.send("Request to play chess timed out.")
+      
+    if component[0].emojis == "❎":
+      await ctx.send("Request to play chess denied.")
     
     channel = await utils.get_setup_type(ctx, "play", member)
     colors = utils.get_colors(ctx.author, member)
@@ -104,21 +103,15 @@ class Chess(commands.Cog):
     )
     
     
-  @cog_ext.cog_slash(
+  @commands.command(
     name="move",
     description="Allows you to move in a game of chess.",
-    options=[
-      {
-        "name": "move",
-        "description": "The move you want to move in uci format ex: e2e4",
-        "type": 3,
-        "required": True
-      }
-    ]
+    brief="Allows you to move in chess.",
+    usage="<move: uci>"
   )
   async def _move(
     self,
-    ctx: SlashContext,
+    ctx: commands.Context,
     move: str
   ) -> discord.Message:
     if games[ctx.channel.id].get_move_user().id != ctx.author.id:
@@ -130,13 +123,15 @@ class Chess(commands.Cog):
    
   
   
-  @cog_ext.cog_slash(
+  @commands.command(
     name="resign",
-    description="Allows you to resign in a game of chess."
+    description="Allows you to resign in a game of chess.",
+    brief="Resigns in a chess match.",
+    usage=""
   )
   async def _resign(
     self,
-    ctx: SlashContext
+    ctx: commands.Context
   ) -> discord.Message:
     if games[ctx.channel.id].get_move_user().id != ctx.author.id:
       pass
@@ -147,11 +142,12 @@ class Chess(commands.Cog):
   
   @cog_ext.cog_slash(
     name="draw",
+    brief="Requests to draw a game of chess.",
     description="Allows you to draw in a game of chess."
   )
   async def _draw(
     self,
-    ctx: SlashContext
+    ctx: commands.Context
   ) -> discord.Message:
     if games[ctx.channel.id].get_move_user().id != ctx.author.id:
       pass
@@ -160,42 +156,41 @@ class Chess(commands.Cog):
 
     game = games[ctx.channel.id]
      
-    action_row = create_actionrow(
-      create_button(style=ButtonStyle.green, label="Yes", custom_id="yes"),
-      create_button(style=ButtonStyle.red, label="No", custom_id="no"),
+#     action_row = create_actionrow(
+#       create_button(style=ButtonStyle.green, label="Yes", custom_id="yes"),
+#       create_button(style=ButtonStyle.red, label="No", custom_id="no"),
+#     )
+    
+    message_obj = await ctx.send(
+      "{0}, would you like to draw the match against {1}?".format([game.white.user, game.black.user].remove(ctx.author).mention, ctx.author.mention)
     )
     
-    message_obj = await ctx.channel.send(
-      "{0}, would you like to draw the match against {1}?".format([game.white.user, game.black.user].remove(ctx.author).mention, ctx.author.mention),
-      components=[
-        action_row
-      ]
-    )
+    emojis = ["✅", "❎"]
     
-    component = await wait_for_component(self.bot, components=action_row)
+    for emoji in emojis:
+      message_obj.add_reaction(emoji)
     
-    if component.custom_id == "no":
+    try:
+      component = await self.bot.wait_for('reaction_add', check = lambda reaction, user: reaction.message == message_obj and reaction.emoji in emojis and user == [game.white.user, game.black.user].remove(ctx.author), timeout=180)
+    except asyncio.TimeOutError:
+      return await ctx.send("Request to draw timed out.")
+    
+    if component[0].emoji == "❎":
       return await ctx.send("Request to draw denied.")
     return await game.end(2, 0.5, 0.5)
 
-  @cog_ext.cog_slash(
+  @commands.command(
     name="elo",
-    description="Returns the elo of a user.",
-    options = [
-      {
-        "name": "member",
-        "description": "Member you would like to play Chess against.",
-        "type": 6,
-        "required": False
-      } 
-    ]
+    description="Returns the elo of a given chess user.",
+    brief="Returns the elo of a user.",
+    usage="<member>"
   )
   async def _elo(
     self,
-    ctx: SlashContext,
-    member: discord.Member
+    ctx: commands.Context,
+    member: discord.Member = None
   ) -> discord.Message:
-    if not member:
+    if member == None:
       member=ctx.author
     
     if str(member.id) not in db["users"]:
@@ -210,24 +205,18 @@ class Chess(commands.Cog):
       ).set_author(name="Elo for user")
     ) 
 
-  @cog_ext.cog_slash(
+  @commands.command(
     name="profile",
     description="Displays information on a user.",
-    options = [
-      {
-        "name": "member",
-        "description": "The user to display the information of.",
-        "type": 6,
-        "required": False
-      }
-    ]
+    brief="Displays a user's profile.",
+    usage="<member>"
   )
   async def _profile(
     self,
-    ctx: SlashContext,
-    member: discord.Member
+    ctx: commands.Context,
+    member: discord.Member = None
   ) -> discord.Message:
-    if not member:
+    if member == None:
       member=ctx.author
     
     if str(member.id) not in db["users"]:
@@ -238,42 +227,48 @@ class Chess(commands.Cog):
       status = "___"
       status_color = discord.Colour.green()
     else:
-      if 'elo' in db["users"][str(member.id)]:
-        elo = db["users"][str(member.id)]['elo']
-      else:
-        elo = 600
+#       if 'elo' in db["users"][str(member.id)]:
+#         elo = db["users"][str(member.id)]['elo']
+#       else:
+#         elo = 600
 
-      if 'wins' in db["users"][str(member.id)]:
-        wins = db["users"][str(member.id)]['wins']
-      else:
-        wins = 0
+#       if 'wins' in db["users"][str(member.id)]:
+#         wins = db["users"][str(member.id)]['wins']
+#       else:
+#         wins = 0
 
-      if 'losses' in db["users"][str(member.id)]:
-        losses = db["users"][str(member.id)]['losses']
-      else:
-         losses = 0
+#       if 'losses' in db["users"][str(member.id)]:
+#         losses = db["users"][str(member.id)]['losses']
+#       else:
+#          losses = 0
 
-      if 'draws' in db["users"][str(member.id)]:
-        draws = db["users"][str(member.id)]['draws']
+#       if 'draws' in db["users"][str(member.id)]:
+#         draws = db["users"][str(member.id)]['draws']
 
-      else: 
-        draws = 0
+#       else: 
+#         draws = 0
 
-      if 'status' in db["users"][str(member.id)]:
-        status = db["users"][str(member.id)]['status']
-      else:
-         status = "___"
+#       if 'status' in db["users"][str(member.id)]:
+#         status = db["users"][str(member.id)]['status']
+#       else:
+#          status = "___"
 
-      if 'status_color' in db["users"][str(member.id)]:
-        status_color = db["users"][str(member.id)]['status_color']
-      else:
-         status_color = discord.Colour.green()
-      
-      
+#       if 'status_color' in db["users"][str(member.id)]:
+#         status_color = db["users"][str(member.id)]['status_color']
+#       else:
+#          status_color = discord.Colour.green()
+
+        
+        # inline if else mess essentially defaults unset values.
+        elo = 600 if 'elo' not in db["users"][str(member.id)] else elo = db["users"][str(member.id)]['elo']
+        wins = 0 if 'wins' not in db["users"][str(member.id)] else wins = db["users"][str(member.id)]['wins']
+        losses = 0 if 'losses' not in db["users"][str(member.id)] else losses = db["users"][str(member.id)]['losses']
+        draws = 0 if 'draws' not in db["users"][str(member.id)] else draws = db["users"][str(member.id)]['draws']
+        status = "___" if 'status' not in db["users"][str(member.id)] else status = db["users"][str(member.id)]['status']
+        status_color = discord.Colour.green() if 'status_color' not in db["users"][str(member.id)] else status_color = db["users"][str(member.id)]['status_color']
+  
     
-    #premptive message (made in class I'll finish this later)
-    
-    return await ctx.channel.send(
+    return await ctx.send(
       embed=discord.Embed(
         description=status,
         color=status_color
